e79df2e70519a074e12443bfd89af58e3d543bb8
phucsbui <duyphucbui522@gmail.com>
2024-09-23 13:36:52 +0700

    Add project files.

---
+++ Week1/Presentation/AnimalDetail.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Week1.Presentation
+{
+    partial class AnimalDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            AnimalTextBox = new TextBox();
+            AddAnimalDetail = new Button();
+            CancelAnimalDetail = new Button();
+            AnimalsComboBox = new ComboBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Type: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Quantity: ";
+            // 
+            // AnimalTextBox
+            // 
+            AnimalTextBox.Location = new Point(114, 136);
+            AnimalTextBox.Name = "AnimalTextBox";
+            AnimalTextBox.Size = new Size(138, 27);
+            AnimalTextBox.TabIndex = 4;
+            AnimalTextBox.TextChanged += AnimalTextBox_TextChanged;
+            // 
+            // AddAnimalDetail
+            // 
+            AddAnimalDetail.Location = new Point(36, 196);
+            AddAnimalDetail.Name = "AddAnimalDetail";
+            AddAnimalDetail.Size = new Size(94, 29);
+            AddAnimalDetail.TabIndex = 5;
+            AddAnimalDetail.Text = "Add";
+            AddAnimalDetail.UseVisualStyleBackColor = true;
+            AddAnimalDetail.Click += AddAnimalDetail_Click;
+            // 
+            // CancelAnimalDetail
+            // 
+            CancelAnimalDetail.Location = new Point(158, 196);
+            CancelAnimalDetail.Name = "CancelAnimalDetail";
+            CancelAnimalDetail.Size = new Size(94, 29);
+            CancelAnimalDetail.TabIndex = 6;
+            CancelAnimalDetail.Text = "Cancel";
+            CancelAnimalDetail.UseVisualStyleBackColor = true;
+            CancelAnimalDetail.Click += CancelAnimalDetail_Click;
+            // 
+            // AnimalsComboBox
+            // 
+            AnimalsComboBox.FormattingEnabled = true;
+            AnimalsComboBox.Location = new Point(114, 79);
+            AnimalsComboBox.Name = "AnimalsComboBox";
+            AnimalsComboBox.Size = new Size(138, 28);
+            AnimalsComboBox.TabIndex = 7;
+            AnimalsComboBox.SelectedIndexChanged += AnimalsComboBox_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(50, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(173, 38);
+            label3.TabIndex = 8;
+            label3.Text = "Add Animals";
+            label3.Click += label3_Click;
+            // 
+            // AnimalDetail
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(279, 269);
+            Controls.Add(label3);
+            Controls.Add(AnimalsComboBox);
+            Controls.Add(CancelAnimalDetail);
+            Controls.Add(AddAnimalDetail);
+            Controls.Add(AnimalTextBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            Name = "AnimalDetail";
+            Text = "AnimalDetail";
+            FormClosing += AnimalDetail_FormClosing;
+            Load += AnimalDetail_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label2;
+        private TextBox AnimalTextBox;
+        private Button AddAnimalDetail;
+        private Button CancelAnimalDetail;
+        private ComboBox AnimalsComboBox;
+        private Label label3;
+    }
+}
---
+++ Week1/Presentation/AnimalManager.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Week1.Presentation
+{
+    partial class AnimalManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AnimalDataGridView = new DataGridView();
+            OpenAnimalDetailBtn = new Button();
+            SpeakBtn = new Button();
+            BornBtn = new Button();
+            ProduceMilkBtn = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)AnimalDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // AnimalDataGridView
+            // 
+            AnimalDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            AnimalDataGridView.Location = new Point(21, 89);
+            AnimalDataGridView.Name = "AnimalDataGridView";
+            AnimalDataGridView.RowHeadersWidth = 51;
+            AnimalDataGridView.Size = new Size(458, 209);
+            AnimalDataGridView.TabIndex = 0;
+            AnimalDataGridView.CellContentClick += AnimalDataGridView_CellContentClick;
+            // 
+            // OpenAnimalDetailBtn
+            // 
+            OpenAnimalDetailBtn.Location = new Point(485, 89);
+            OpenAnimalDetailBtn.Name = "OpenAnimalDetailBtn";
+            OpenAnimalDetailBtn.Size = new Size(112, 44);
+            OpenAnimalDetailBtn.TabIndex = 1;
+            OpenAnimalDetailBtn.Text = "Add Animals";
+            OpenAnimalDetailBtn.UseVisualStyleBackColor = true;
+            OpenAnimalDetailBtn.Click += OpenAnimalDetailBtn_Click;
+            // 
+            // SpeakBtn
+            // 
+            SpeakBtn.Location = new Point(485, 139);
+            SpeakBtn.Name = "SpeakBtn";
+            SpeakBtn.Size = new Size(112, 44);
+            SpeakBtn.TabIndex = 2;
+            SpeakBtn.Text = "Speak";
+            SpeakBtn.UseVisualStyleBackColor = true;
+            SpeakBtn.Click += SpeakBtn_Click;
+            // 
+            // BornBtn
+            // 
+            BornBtn.Location = new Point(485, 189);
+            BornBtn.Name = "BornBtn";
+            BornBtn.Size = new Size(112, 44);
+            BornBtn.TabIndex = 4;
+            BornBtn.Text = "Born";
+            BornBtn.UseVisualStyleBackColor = true;
+            BornBtn.Click += BornBtn_Click;
+            // 
+            // ProduceMilkBtn
+            // 
+            ProduceMilkBtn.Location = new Point(485, 239);
+            ProduceMilkBtn.Name = "ProduceMilkBtn";
+            ProduceMilkBtn.Size = new Size(112, 44);
+            ProduceMilkBtn.TabIndex = 5;
+            ProduceMilkBtn.Text = "Produce Milk";
+            ProduceMilkBtn.UseVisualStyleBackColor = true;
+            ProduceMilkBtn.Click += ProduceMilkBtn_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(179, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(266, 46);
+            label1.TabIndex = 6;
+            label1.Text = "Animal Manager";
+            // 
+            // AnimalManager
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(612, 328);
+            Controls.Add(label1);
+            Controls.Add(ProduceMilkBtn);
+            Controls.Add(BornBtn);
+            Controls.Add(SpeakBtn);
+            Controls.Add(OpenAnimalDetailBtn);
+            Controls.Add(AnimalDataGridView);
+            Name = "AnimalManager";
+            Text = "AnimalManager";
+            Load += AnimalManager_Load;
+            ((System.ComponentModel.ISupportInitialize)AnimalDataGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        public DataGridView AnimalDataGridView;
+        private Button OpenAnimalDetailBtn;
+        private Button SpeakBtn;
+        private Button BornBtn;
+        private Button ProduceMilkBtn;
+        private Label label1;
+    }
+}
